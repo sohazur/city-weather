98272e3998151433ac863a3958b886ecf5fa11c2
Sohazur <mdsohazurislam@gmail.com>
2021-12-31 04:04:16 +0400

    Updated JS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
-const loadSports = () => {
-  fetch("https://www.thesportsdb.com/api/v1/json/2/all_sports.php")
+const API_KEY = `5437efdcdf46c4ba32ecd9a864d6a37e`;
+
+loadCityTemp = () => {
+  const city = document.getElementById("city-name").value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+  )
     .then((res) => res.json())
-    .then((data) => displaySports(data));
+    .then((data) => displayCityTemp(data));
 };
 
-const displaySports = (sports) => {
-  console.log(sports.sports);
+displayCityTemp = (city) => {
+  document.getElementById("city-name-display").innerHTML = `
+  <img src="https://openweathermap.org/img/wn/${
+    city.weather[0].icon
+  }@2x.png" alt="" />
+  <h1 class="lead">${city.weather[0].main}</h1>
+        <h1>${city.name}</h1>
+        <h3><span>${kelvinToCelcius(city.main.temp)}</span>&deg;C</h3>
+        
+  `;
+
+  console.log(city);
 };
 
-loadSports();
+kelvinToCelcius = (kelvin) => {
+  celcius = kelvin - 273.16;
+  return celcius.toFixed(2);
+};
